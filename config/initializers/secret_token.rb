0954463b4194630f9c3b61b5b9205b2fcb44af5c
lifang <santhe_2004@126.com>
2013-09-12 15:28:09 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QxgManage::Application.config.secret_token = 'e032093f454213d511c3ac36ed02072070678da69c49675b8e2b3a7aeb6031ac6a22e0847719509c2e6a03b7ce2cd8ef899e1023fc015fa58a6bbb13b8b6bba1'
